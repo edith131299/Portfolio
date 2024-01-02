987e375159e0fe0f9d9d18e57a291e016aa15e41
edith131299 <sudakarvenkat023@gmail.com>
2024-01-02 23:05:59 +0530

    backend config

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
+app.listen("3001", () => {
+  console.log(`Server running at port:3001`);
+});
